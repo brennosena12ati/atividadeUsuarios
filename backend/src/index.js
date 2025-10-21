0d93528100d949ac620b18483d653aa03c4be690
Brenno Sena Zavataro <brennounasp@gmail.com>
2025-10-21 18:20:36 -0300

    FIM

---
+++ backend/src/index.js
@@ -0,0 +1,17 @@
+import express from "express"
+import cors from "cors"
+import { people } from "./people.js"
+
+const app = express()
+const port = 3333
+
+app.use(cors())
+//GET, POST, PUT, PATCH, DELETE.
+app.get("/", (request, response) => {
+    response.json(people)
+})
+
+//DEIXAR O SERVIDOR ONLINE
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta: ${port}`)
+})
