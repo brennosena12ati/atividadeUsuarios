bd283fab38f11daa6b0f5820cfa8252b9ecdab31
Brenno Sena Zavataro <brennounasp@gmail.com>
2025-11-06 22:20:27 -0300

    Envio de informações do frontend para o backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,9 +6,25 @@ const app = express()
 const port = 3333
 
 app.use(cors())
+app.use(express.json())
 //GET, POST, PUT, PATCH, DELETE.
 app.get("/", (request, response) => {
     response.json(people)
+})
+
+app.post("/cadastrar", (request, response) => {
+    const { name, email, age, passsword } = request.body.user
+
+    console.log(`
+        Nome: ${name},
+        E-mail: ${email},
+        Idade: ${age},
+        Senha: ${passsword}
+        `)
+
+        //guardar usuário no banco de dados
+
+        response.status(201).json({ message: "Usuário cadastrado com sucesso!" })
 })
 
 //DEIXAR O SERVIDOR ONLINE
